96ca95a1810e443ad9163be6472e11b008531d8d
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-01-30 01:38:46 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Flask-DotEnv',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/grauwoelfchen/flask-dotenv/',
     license='BSD',
     author='Yasuhiro Asaka',
